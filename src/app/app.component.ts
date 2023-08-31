f78a2bb57a78129149dd61858e257af7014f9b65
Franak007 <franck.pertosa@gmail.com>
2023-08-31 15:13:46 +0200

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,21 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome Angular {{pokemonList[1]}}</h1>`
+  templateUrl: ``
 })
 export class AppComponent implements OnInit {
   title: string = 'pokemon-app';
-  pokemonList = ["Pikachu", "Salamèche", "Lippoutou"];
+  pokemonList: string[] = ["Pikachu", "Salamèche", "Lippoutou"];
 
   ngOnInit(): void {
     console.log("Je viens de charger!");
     console.table(this.pokemonList);
+
+    this.selectPokemon(this.pokemonList[0]);
+
+  }
+
+  selectPokemon(pokemon: string): void {
+    console.log("Vous avez sélectionné ce Pokemon : " + pokemon)
   }
 }
