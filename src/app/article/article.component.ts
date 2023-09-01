cc372d85ebfdc7da4944f8ce269d555eec6d35e7
Franak007 <franck.pertosa@gmail.com>
2023-09-01 15:18:06 +0200

    incrémentation component article avec des articles et logique entre vue et logique article

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -2,14 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-article',
-  template: `
-    <p>
-      article works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: `article.component.html`,
+  styles: []
 })
 export class ArticleComponent {
+
+  articles: string[] = [
+      "Pokemon article 1",
+      "Pokemon article 2",
+      "Pokemon article 3",
+      "Pokemon article 4",
+      "Pokemon article 5",
+      "Pokemon article 6"
+  ]
+
+  showArticle(article: string)  {
+    console.log("Vous avez sélectionné cet article : " + article);
+  }
+
+  resultForm(form: string)  {
+    console.log("Vous avez écrit dans le formulaire ceci : " + form);
+  }
 
 }
