da0943e352ce9e04d6f76b492efb45aef871de71
Franak007 <franck.pertosa@gmail.com>
2023-09-07 14:40:01 +0200

    formulaire opérationnel avec vérif nb caractères

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,8 +7,29 @@ import {NgForm} from "@angular/forms";
   styles: []
 })
 export class ContactComponent {
+
+  emojiName: string = "🚫";
+  emojiPassword: string = "🚫";
+
   onSubmit(form: NgForm) {
+    let username: string = form.value.username;
+    let password: string = form.value.password;
+
+    if (username.length <= 2) {
+      this.emojiName = "🚫";
+    } else {
+      this.emojiName = "🟢"
+    }
+
+    if (password.length > 6) {
+      this.emojiPassword = "🚫";
+    } else {
+      this.emojiPassword = "🟢"
+    }
+
+    console.log(form.value.username);
     console.log(form.value);
+    form.resetForm();
   }
 
   protected readonly onsubmit = onsubmit;
